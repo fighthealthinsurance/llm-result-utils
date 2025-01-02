3c98c5c66c5d25307881842263315a457643a78c
Holden Karau <holden@pigscanfly.ca>
2025-01-01 21:37:54 -0800

    improve tests

--- tests/test_bad_urls.py
+++ tests/test_bad_urls.py
@@ -1,14 +1,13 @@
 from unittest import TestCase
-from fighthealthinsurance.utils import url_fixer
-
+from llm_result_utils.cleaner_utils import CleanerUtils
 
 class TestBadURLs(TestCase):
 
     def test_none(self):
-        fixed = url_fixer(None)
+        fixed = CleanerUtils.url_fixer(None)
         self.assertEqual(None, fixed)
 
     def test_badtla(self):
         # This will break if google adds a fart page but ehhh seams legit
-        fixed = url_fixer("http://www.google.com http://www.google.com/farts")
+        fixed = CleanerUtils.url_fixer("http://www.google.com http://www.google.com/farts")
         self.assertEqual("http://www.google.com ", fixed)
